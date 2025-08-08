59a863a1d20466e444b8a548f81517eeaecc9ecf
Aman Tiwari <at77245@gmail.com>
2025-08-08 09:11:53 +0530

    removed the api

--- amplify/api/resource.ts
+++
@@ -1,7 +0,0 @@
-// backend/api/resource.ts
-import { defineFunction } from "@aws-amplify/backend";
-
-export const helloWorldFunction = defineFunction({
-  name: "hello-world",
-  entry: "./function/hello-world.ts", // path to your handler
-});
